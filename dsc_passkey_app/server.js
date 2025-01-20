1d95a7794f6df1b21b99a3155bedac5521194863
Nnaemeka Emejulu <158416054+gl0bal74@users.noreply.github.com>
2025-01-20 13:25:53 -0600

    added timeout and reset functionality

--- dsc_passkey_app/server.js
+++ dsc_passkey_app/server.js
@@ -7,14 +7,24 @@ let isPasskeyPressed = false;
 app.use(express.json());
 
 app.post('/press-passkey', (req, res) => {
-  isPasskeyPressed = true;
-  res.status(200).send('Passkey pressed');
+    isPasskeyPressed = true;
+    res.status(200).send('Passkey pressed');
+
+    // Reset passkey status after 5 minutes (300000 milliseconds)
+    setTimeout(() => {
+        isPasskeyPressed = false;
+    }, 300000);
+});
+
+app.post('/reset-passkey', (req, res) => {
+    isPasskeyPressed = false;
+    res.status(200).send('Passkey reset');
 });
 
 app.get('/check-passkey', (req, res) => {
-  res.json({ pressed: isPasskeyPressed });
+    res.json({ pressed: isPasskeyPressed });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
